08b3dde8c8903dfd7a6afcb07bd5c2652fd8afe1
0x8a00e6 <84236633+ApostleOfHate@users.noreply.github.com>
2024-08-02 20:44:05 +0400

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-5">
+      <h1 className="text-5xl mb-2">ALL EYES ON</h1>
+      <h1 className="text-5xl red-text">YOU</h1>
+    </main>
+  );
+}
