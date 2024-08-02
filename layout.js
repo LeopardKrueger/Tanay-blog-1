963b4e1a7a172609d77a0afa2d989fa6b1022214
LeopardKrueger <110789857+LeopardKrueger@users.noreply.github.com>
2024-08-02 21:52:02 +0400

    Update layout.js
    tgegan

--- layout.js
+++ layout.js
@@ -1,17 +1,24 @@
-import './globals.css'
-import { Chivo_Mono } from 'next/font/google'
+import './globals.css';
+import { Chivo_Mono } from 'next/font/google';
 
-const chivo = Chivo_Mono({ subsets: ['latin'] })
+const chivo = Chivo_Mono({
+  subsets: ['latin'],
+});
 
 export const metadata = {
   title: 'Letters of Thought',
   description: 'Student views. Global issues.',
-}
+  keywords: 'student blog, global issues', // add keywords for SEO
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={chivo.className}>{children}</body>
     </html>
-  )
+  );
 }
